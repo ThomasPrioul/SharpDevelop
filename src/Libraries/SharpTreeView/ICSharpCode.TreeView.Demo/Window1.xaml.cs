1b9db71843195d114ed761304ed88fb6b5b417fc
Thomas Prioul <thomas.prioul@outlook.com>
2019-03-11 08:53:36 +0100

    Fix build...

--- src/Libraries/SharpTreeView/ICSharpCode.TreeView.Demo/Window1.xaml.cs
+++ src/Libraries/SharpTreeView/ICSharpCode.TreeView.Demo/Window1.xaml.cs
@@ -1,14 +1,14 @@
 ﻿// Copyright (c) 2014 AlphaSierraPapa for the SharpDevelop Team
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this
 // software and associated documentation files (the "Software"), to deal in the Software
 // without restriction, including without limitation the rights to use, copy, modify, merge,
 // publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
 // to whom the Software is furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all copies or
 // substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
 // INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
 // PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
@@ -58,12 +58,12 @@ namespace ICSharpCode.TreeView.Demo
 		//    Debug.WriteLine(sb.ToString());
 		//}
 
-		public static Image LoadIcon(string name)
+		public static ImageSource LoadIcon(string name)
 		{
 			var frame = BitmapFrame.Create(new Uri("pack://application:,,,/Images/" + name, UriKind.Absolute));
-			Image result = new Image();
-			result.Source = frame;
-			return result;
+			//Image result = new Image();
+			//result.Source = frame;
+			return frame;
 		}
 	}
 }
